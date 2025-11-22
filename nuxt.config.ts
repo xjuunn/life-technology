603a1aa4496079c6bcfe4f5466036ac3593ea2b0
Junhsiun <xjuunn@gmail.com>
2025-11-22 10:27:50 +0800

    feat:init

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+      ]
+    }
+  },
+  modules: ['@nuxt/content', '@nuxt/icon']
+})
