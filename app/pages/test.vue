8a94355f6ee1f8eba45dace67ce23bbd1e0389e6
Junhsiun <xjuunn@gmail.com>
2025-11-27 19:49:48 +0800

    fix:修复博客接口的类型错误

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,30 +1,26 @@
 <template>
-  <common-editor v-model="data" />
-  <pre class="text-sm mt-4">{{ JSON.stringify(data, null, 2) }}</pre>
   <editor-content :editor="editor" class="prose max-w-none mt-4" />
 </template>
-<script setup lang="ts">
-import { EditorContent, useEditor } from "@tiptap/vue-3"
-import StarterKit from "@tiptap/starter-kit"
-import { watch } from "vue"
-const data = ref<any>(null)
-const props = defineProps<{
-  content: any
-}>()
 
+<script setup lang="ts">
+import { EditorContent, useEditor} from "@tiptap/vue-3"
+import StarterKit from "@tiptap/starter-kit"
 const editor = useEditor({
   editable: false,
   extensions: [StarterKit],
-  content: props.content
-})
-
-watch(
-  () => data.value,
-  (newValue) => {
-    if (editor.value && newValue) {
-      editor.value.commands.setContent(newValue)
+  content: {
+  "type": "doc",
+  "content": [
+    {
+      "type": "paragraph",
+      "content": [
+        {
+          "type": "text",
+          "text": "123123"
+        }
+      ]
     }
-  },
-  { deep: true }
-)
+  ]
+},
+})
 </script>
