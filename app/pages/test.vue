5fecead0568b18aac5ef2067eb60f08f6cede307
Junhsiun <xjuunn@gmail.com>
2025-12-06 13:39:41 +0800

    docs:新增gsap llms

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,10 +1,10 @@
 <template>
   <div ref="wrapperRef" class="h-screen overflow-auto w-screen">
     <div ref="contentRef" class="h-[300vh] flex gap-3 items-center justify-center">
-      <div class="w-24 h-24 rounded-lg bg-base-content"></div>
-      <div class="w-24 h-24 rounded-lg bg-base-content"></div>
-      <div class="w-24 h-24 rounded-lg bg-base-content"></div>
-      <div class="w-24 h-24 rounded-lg bg-base-content"></div>
+      <div data-speed="0.1"  data-lag="0.1" class="w-24 h-24 rounded-lg bg-base-content"></div>
+      <div data-speed="0.2"  data-lag="0.2" class="w-24 h-24 rounded-lg bg-base-content"></div>
+      <div data-speed="0.3"  data-lag="0.9" class="w-24 h-24 rounded-lg bg-base-content"></div>
+      <div data-speed="0.4"  data-lag="0.2" class="w-24 h-24 rounded-lg bg-base-content"></div>
     </div>
   </div>
 </template>
@@ -22,7 +22,8 @@ useGsap((ctx) => {
   smooter = ScrollSmoother.create({
     content: contentRef.value,
     wrapper: wrapperRef.value,
-    smooth: 1
+    smooth: 1,
+    effects: true
   })
 })
 
